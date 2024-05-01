5888fb0b800d0aede585b6d1b8f4179194f160d7
ahddl <101929817+ahddl@users.noreply.github.com>
2024-05-01 11:43:10 +0900

    v.1.0.2

--- app/src/main/java/com/example/dia_v102/RecyclerDietItem.java
+++ app/src/main/java/com/example/dia_v102/RecyclerDietItem.java
@@ -15,7 +15,7 @@ public class RecyclerDietItem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recycler_diet_item);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearItem), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
