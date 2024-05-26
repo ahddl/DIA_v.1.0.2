388b0bd6ccc0ef7f3c93bd31c8c787aa2c9bf89a
ahddl <101929817+ahddl@users.noreply.github.com>
2024-05-26 21:24:24 +0900

    2

--- app/src/main/java/com/example/dia_v102/InputBasicData.java
+++ app/src/main/java/com/example/dia_v102/InputBasicData.java
@@ -44,8 +44,6 @@ public class InputBasicData extends AppCompatActivity {
         });
 
 
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
