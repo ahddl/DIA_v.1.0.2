4f0cde3c65576753a6d82c306b87ad3f0debdd47
ahddl <101929817+ahddl@users.noreply.github.com>
2024-09-08 13:16:39 +0900

    DietImageDao, DietImage 오류 주석처리

--- app/src/main/java/com/example/dia_v102/database/AppDatabase.java
+++ app/src/main/java/com/example/dia_v102/database/AppDatabase.java
@@ -4,14 +4,14 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-import com.example.dia_v102.entities.DietImage;
-import com.example.dia_v102.dao.DietImageDao;
+//import com.example.dia_v102.entities.DietImage;
+//import com.example.dia_v102.dao.DietImageDao;
 import com.example.dia_v102.entities.Food_menu;
 import com.example.dia_v102.dao.Food_menuDao;
 
-@Database(entities = {DietImage.class, Food_menu.class}, version = 2)
+@Database(entities = {Food_menu.class}, version = 2)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract DietImageDao dietImageDao();
+    //public abstract DietImageDao dietImageDao();
     public abstract Food_menuDao food_menuDao();
 }
