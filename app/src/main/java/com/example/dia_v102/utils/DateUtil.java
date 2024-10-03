f4e56cd31468445ce3c2c6cdb7786b8d60134d01
ahddl <101929817+ahddl@users.noreply.github.com>
2024-10-04 03:59:48 +0900

    날짜출력형식변경

--- app/src/main/java/com/example/dia_v102/utils/DateUtil.java
+++ app/src/main/java/com/example/dia_v102/utils/DateUtil.java
@@ -9,13 +9,13 @@ public class DateUtil {
 
     // 날짜를 "yyyyMMdd" 형식의 문자열로 변환
     public static String dateToString(Date date) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.getDefault()); // 원하는 형식으로 변경 가능
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()); // 원하는 형식으로 변경 가능
         return sdf.format(date);
     }
 
     // "yyyyMMdd" 형식의 문자열을 Date 객체로 변환
     public static Date stringToDate(String dateString) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.getDefault()); // 문자열 형식과 일치해야 함
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()); // 문자열 형식과 일치해야 함
         return sdf.parse(dateString);
     }
 
