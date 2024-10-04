a234486ad86934576592c6171ab6ca96cd29f679
inte168 <intelligent1001@chungbuk.ac.kr>
2024-10-04 16:23:43 +0900

    당화혈색소 관련 코드 주석 해제 및 수정

--- app/src/main/java/com/example/dia_v102/TabGlycated.java
+++ app/src/main/java/com/example/dia_v102/TabGlycated.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,6 +27,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -46,62 +46,56 @@ public class TabGlycated extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // fragment_tab_bloodsugar 레이아웃을 사용하여 view를 생성
+        // fragment_tab_glycated 레이아웃을 사용하여 view를 생성
         View view = inflater.inflate(R.layout.tab_glycated, container, false);
         Func_InfoBox FinfoBox = new Func_InfoBox();
 
-        /*// UI 요소 참조
-        EditText bloodSugarInput = view.findViewById(R.id.glycated);
+        // UI 요소 참조
+        EditText GlycatedInput = view.findViewById(R.id.glycated);
         Button saveButton = view.findViewById(R.id.save_button_gly);
         recyclerView = view.findViewById(R.id.recycler_view_gly);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         // 저장 버튼 클릭 리스너
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //String tag2 = dropdownMenu.getSelectedItem().toString();
-                double sugar = Double.parseDouble(bloodSugarInput.getText().toString());
-                FinfoBox.saveInfoBox(CurrUser.getUid(), null, HourNMin(), "당화혈색소", glycated);
-                Toast.makeText(requireContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
-                loadDiabeteData(DateUtil.dateToString(new Date()));
-            }
+        saveButton.setOnClickListener(v -> {
+            //String tag2 = dropdownMenu.getSelectedItem().toString();
+            double glycated = Double.parseDouble(GlycatedInput.getText().toString());
+            FinfoBox.saveInfoBox(CurrUser.getUid(), null, HourNMin(), "당화혈색소", null, glycated);
+            Toast.makeText(requireContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
+            loadGlycatedData(DateUtil.dateToString(new Date()));
         });
 
         // 데이터 로드
-        loadDiabeteData(DateUtil.dateToString(new Date()));*/
+        loadGlycatedData(DateUtil.dateToString(new Date()));
 
         return view;
     }
 
-    /*// Firestore에서 데이터를 로드하여 RecyclerView에 표시
-    private void loadDiabeteData(String date) {
+    // Firestore에서 데이터를 로드하여 RecyclerView에 표시
+    private void loadGlycatedData(String date) {
         infoBox = new Func_InfoBox();
         userID = CurrUser.getUid();
 
-        executorService.execute(() -> {
+        executorService.execute(() -> infoBox.loadInfoBox(userID, "당화혈색소", date, new Func_InfoBox.OnDataReceivedListener(){
+            @Override
+            public void onDataReceived(List<InfoBox> infoBoxList){
+                Log.d("BoxOut", "Success");
 
-            infoBox.loadInfoBox(userID, "혈당", date, new Func_InfoBox.OnDataReceivedListener(){
-                @Override
-                public void onDataReceived(List<InfoBox> infoBoxList){
-                    Log.d("BoxOut", "Success");
-
-                    for (InfoBox infoBox : infoBoxList) {
-                        Log.d("BoxOut", "InfoBox Data: " + infoBox.getTime()); // infoBox의 toString() 메서드를 사용하여 데이터를 출력
-                    }
-                    mainThreadHandler.post(()->{
-                        adapter = new BSAdapter(infoBoxList);
-                        recyclerView.setAdapter(adapter);
-
-                    });
+                for (InfoBox infoBox : infoBoxList) {
+                    Log.d("BoxOut", "InfoBox Data: " + infoBox.getTime()); // infoBox의 toString() 메서드를 사용하여 데이터를 출력
                 }
+                mainThreadHandler.post(()->{
+                    adapter = new BSAdapter(infoBoxList);
+                    recyclerView.setAdapter(adapter);
 
-                @Override
-                public void onDataFailed(Exception exception) {
-                    Log.d("BoxOut", exception.getMessage());
-                }
-            });
-        });
+                });
+            }
+
+            @Override
+            public void onDataFailed(Exception exception) {
+                Log.d("BoxOut", Objects.requireNonNull(exception.getMessage()));
+            }
+        }));
 
     }
 
@@ -111,5 +105,5 @@ public class TabGlycated extends Fragment {
         int min = calendar.get(Calendar.MINUTE);
 
         return String.format(Locale.getDefault(), "%02d-%02d", hour, min);
-    }*/
+    }
 }
