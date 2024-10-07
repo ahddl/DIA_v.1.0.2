f633fc56a1f9aaf4b3a13a957f76e8c77e86af6a
inte168 <intelligent1001@chungbuk.ac.kr>
2024-10-07 19:26:26 +0900

    비밀번호 변경 기능 추가

---
+++ app/src/main/java/com/example/dia_v102/FindPW.java
@@ -0,0 +1,71 @@
+package com.example.dia_v102;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FindPW extends AppCompatActivity {
+    private EditText editTextEmail;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.findpw);
+
+        // FirebaseAuth 초기화
+        auth = FirebaseAuth.getInstance();
+
+        // UI 요소 초기화
+        editTextEmail = findViewById(R.id.editTextEmail);
+        Button resetPWButton = findViewById(R.id.resetPasswordButton);
+
+        /*뒤로 가기 버튼 -- mainactivity 로 이동*/
+        Button back1 = findViewById(R.id.back1);
+        back1.setOnClickListener(v -> {
+            Intent signupintent = new Intent(FindPW.this, MainActivity.class);
+            setResult(RESULT_OK, signupintent);
+            finish();
+        });
+
+        // 버튼 클릭 리스너 설정
+        resetPWButton.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString().trim();
+
+            if (email.isEmpty()) {
+                Toast.makeText(FindPW.this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //이메일 유효 확인
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                editTextEmail.setError("유효한 이메일을 입력하세요.");
+                return;
+            }
+            sendPasswordResetEmail(email);
+        });
+    }
+
+    // 비밀번호 재설정 이메일 보내기
+    private void sendPasswordResetEmail(String emailAddress) {
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // 이메일 전송 성공 시
+                        Log.d("PasswordReset", "비밀번호 재설정 이메일 전송 완료");
+                        Toast.makeText(FindPW.this, "비밀번호 재설정 이메일이 전송되었습니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 이메일 전송 실패 시
+                        Log.w("PasswordReset", "비밀번호 재설정 이메일 전송 실패", task.getException());
+                        Toast.makeText(FindPW.this, "이메일 전송 실패. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/dia_v102/MainActivity.java
+++ app/src/main/java/com/example/dia_v102/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.dia_v102;
 
-import android.util.Log;
-import androidx.annotation.NonNull;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,9 +14,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.dia_v102.databaseF.Func_UserInfo;
 import com.example.dia_v102.databaseF.UserInfo;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -34,7 +28,6 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private EditText loginID, loginPW;
     private FirebaseAuth mAuth;
-    // 클래스 멤버 변수로 선언
     private List<Food_menu> foodMenus;
     private FirebaseUser currentUser;
 
@@ -60,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 DatabaseProvider.parseCsvAndInsertToDB(this, R.raw.food_data37);
                 //Log.d("ROOMDB", "Complete!");
             }
-            /*
+            /* 나중에 확인용.
             else {
                 // DB가 이미 채워져 있음을 알리는 메시지
                 //Log.d("DBROOM", "Full DB");
@@ -74,93 +67,59 @@ public class MainActivity extends AppCompatActivity {
 
         // 자동 로그인
         mAuth = FirebaseAuth.getInstance();
-        currentUser = mAuth.getCurrentUser(); //로그인된 유저 있는지 확인.
+        currentUser = mAuth.getCurrentUser(); //로그인 된 유저 확인.
         if(currentUser != null){
             CompleteLogin(currentUser);
         }
 
-        //로그인 버튼 -- ID와 PW 입력 후 로그인 버튼 누르면 로그인 완료 안내와 함께 메인 페이지(nav)로 넘어감
+        //로그인 버튼 -- ID와 PW 입력 후 로그인 버튼 누르면 로그인 완료 안내와 함께 메인 페이지(MainActivity2) 넘어감
         Button login = findViewById(R.id.btnlogin);
         loginID = findViewById(R.id.loginID);
         loginPW = findViewById(R.id.loginPW);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // EditText-입력된 text 가져오기
-                String id = loginID.getText().toString().trim();
-                String pw = loginPW.getText().toString().trim();
+        login.setOnClickListener(v -> {
+            // EditText-입력된 text 가져오기
+            String id = loginID.getText().toString().trim();
+            String pw = loginPW.getText().toString().trim();
 
-                if (id.isEmpty() || pw.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "정확한 ID/PW를 입력하세요.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                // Sign in the user
-                mAuth.signInWithEmailAndPassword(id, pw)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success
-                                    Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                                    currentUser = mAuth.getCurrentUser();
-                                    CompleteLogin(currentUser);
-                                } else {
-                                    // Sign in failed
-                                    Toast.makeText(MainActivity.this, "로그인 불가: ID/PW를 확인해주세요.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-
-                /*
-                //AlertDialog를 생성하고 설정
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Login Information");
-                builder.setMessage("ID: ", id, "\nPW: ", pw);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 확인 버튼 클릭 시 대화창 닫기
-                        dialog.dismiss();
-                    }
-                });
-
-
-                // AlertDialog를 보여주기
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                */
-
-
+            if (id.isEmpty() || pw.isEmpty()) {
+                Toast.makeText(MainActivity.this, "정확한 ID/PW를 입력하세요.", Toast.LENGTH_SHORT).show();
+                return;
             }
+
+            // Sign in the user
+            mAuth.signInWithEmailAndPassword(id, pw)
+                    .addOnCompleteListener(MainActivity.this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                            currentUser = mAuth.getCurrentUser();
+                            CompleteLogin(currentUser);
+                        } else {
+                            // Sign in failed
+                            Toast.makeText(MainActivity.this, "로그인 불가: ID/PW를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
 
         /*회원가입 버튼 -- 누르면 Signup->InputBasicData->InputTime->InputWeek->Diatype 입력 완료 후
         다시 main으로 돌아와서 로그인 하면 메인페이지(nav)로 넘어감*/
 
         Button join_button = findViewById(R.id.joinbotton);
-        join_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, InputSignup.class);
-                startActivity(intent);
-            }
+        join_button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, InputSignup.class);
+            startActivity(intent);
         });
 
         //나중에 카카오 로그인 버튼으로 사용할 것임
         //임시로 일단 눌리면 메인화면(nav) 나타나게 설정
         Button googlelogin = findViewById(R.id.googlelogin);
-        googlelogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-                startActivity(intent);
-            }
+        googlelogin.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            startActivity(intent);
         });
 
 
-/*
-        *//*아이디 찾기 버튼 -- 아이디 찾기 클래스 없음. 일단 주석 처리*//*
+        /*아이디 찾기 버튼 -- 아이디 찾기 클래스 없음. 일단 주석 처리
         Button findID = findViewById(R.id.findID);
 
         findID.setOnClickListener(new View.OnClickListener() {
@@ -171,17 +130,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        *//*비밀번호 재설정 버튼 -- 비밀번호 재설정 클래스 없음. 일단 주석 처리*//*
+        */
+        //비밀번호 재설정 버튼
         Button findPW = findViewById(R.id.findPW);
 
-        findPW.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FindPW.class);
-                startActivity(intent);
-            }
+        findPW.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FindPW.class);
+            startActivity(intent);
         });
-*/
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -201,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
                 UserSet.setESub(userInfo.eMailSub);
                 UserSet.setHeight(userInfo.height);
                 UserSet.setWeight(userInfo.weight);
-                UserSet.setAge(userInfo.age);
                 UserSet.setAge(userInfo.age);
                 UserSet.setGender(userInfo.gender.charAt(0));
                 UserSet.setType(userInfo.type.charAt(0));
