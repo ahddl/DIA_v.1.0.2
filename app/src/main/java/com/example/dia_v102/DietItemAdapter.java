28662a651e39afce223a4f4d43cadb701c8f4b71
inte168 <intelligent1001@chungbuk.ac.kr>
2024-10-11 14:20:15 +0900

    dietItem에 파이차트 추가

--- app/src/main/java/com/example/dia_v102/DietItemAdapter.java
+++ app/src/main/java/com/example/dia_v102/DietItemAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dia_v102.databaseF.FoodCal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.dia_v102.utils.imgUtil;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 public class DietItemAdapter extends RecyclerView.Adapter<DietItemAdapter.DietItemViewHolder> {
 
@@ -37,19 +44,41 @@ public class DietItemAdapter extends RecyclerView.Adapter<DietItemAdapter.DietIt
         FoodCal foodCal = foodCals.get(position);
         holder.menuName.setText(foodCal.getFood());
 
-        String nutriateText = "칼로리: " + foodCal.getCalories() + " kcal"+
+        String nutriaText = "칼로리: " + foodCal.getCalories() + " kcal"+
                 "\n 탄수화물: " + foodCal.getCarbohydrate() + " g" +
                 "\n 단백질: " + foodCal.getProtein() + " g"+
                 "\n 지방: " + foodCal.getFat() + "g"+
                 "\n 콜레스테롤: " + foodCal.getCholesterol() + " mg"+
                 "\n 나트륨: " + foodCal.getSodium() + " mg"+
                 "\n 설탕 당: " + foodCal.getSugar() + " g";
-        holder.nutritionInfo.setText(nutriateText);
+        holder.nutritionInfo.setText(nutriaText);
 
         // 사진 URI를 ImageView에 set
         Context context = holder.itemView.getContext();
         String imgName = foodCal.getImgName();
         imgUtil.setImage(context, holder.photo, imgName);
+
+        // PieChart 설정
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry((float)foodCal.getCarbohydrate(), "탄수화물"));
+        entries.add(new PieEntry((float)foodCal.getProtein(), "단백질"));
+        entries.add(new PieEntry((float)foodCal.getFat(), "지방"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "영양소 구성");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(14f);
+        holder.pieChart.setData(data);
+        holder.pieChart.invalidate(); // refresh
+
+        // 파이차트 클릭 시 nutritionInfo의 visibility를 VISIBLE로 변경
+        holder.infoButton.setOnClickListener(v -> {
+            if (holder.nutritionInfo.getVisibility() == View.GONE) {
+                holder.nutritionInfo.setVisibility(View.VISIBLE);
+            } else {
+                holder.nutritionInfo.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -61,12 +90,17 @@ public class DietItemAdapter extends RecyclerView.Adapter<DietItemAdapter.DietIt
         TextView menuName;
         TextView nutritionInfo;
         ImageView photo;
+        PieChart pieChart;
+        LinearLayout infoButton;
+
 
         public DietItemViewHolder(View itemView) {
             super(itemView);
             menuName = itemView.findViewById(R.id.menu_name);
             nutritionInfo = itemView.findViewById(R.id.nutrition_info);
             photo = itemView.findViewById(R.id.photo);
+            pieChart = itemView.findViewById(R.id.pieChart);
+            infoButton = itemView.findViewById(R.id.infoButton);
         }
     }
 }
