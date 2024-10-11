55d635d6e8b06049b3c1419cdf502de722c1cc0e
inte168 <intelligent1001@chungbuk.ac.kr>
2024-10-11 11:08:55 +0900

    리스너 통합

--- app/src/main/java/com/example/dia_v102/DietOutputNutrient.java
+++ app/src/main/java/com/example/dia_v102/DietOutputNutrient.java
@@ -18,21 +18,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.dia_v102.database.AppDatabase;
 import com.example.dia_v102.database.DatabaseProvider;
 import com.example.dia_v102.entities.Food_menu;
-import com.example.dia_v102.utils.OnFoodMenuRetrievedListener;
 import com.example.dia_v102.utils.imgUtil;
 
 import java.util.List;
 
 import com.example.dia_v102.databaseF.Func_FoodCal;
 
-public class DietOutputNutient extends AppCompatActivity {
+public class DietOutputNutrient extends AppCompatActivity {
 
     TextView outputmenu1;
     TextView nutList;
     TextView caloriesTextView;
     ProgressBar caloriesProgressBar;
 
-    Func_FoodCal foodcal = new Func_FoodCal();
+    Func_FoodCal foodCal = new Func_FoodCal();
 
     String imgUriStr;
     String imgName;
@@ -74,8 +73,8 @@ public class DietOutputNutient extends AppCompatActivity {
         fetchFoodMenu(menu, new OnFoodMenuRetrievedListener() {
             @Override
             public void onFoodMenuRetrieved(Food_menu foodMenu) {
-                // 데이터가 검색된 경우 UI 업데이트
-                String nutriateText = "음식 이름: " + foodMenu.getFood() +
+                // 검색된 경우 UI 업데이트
+                String nutriaText = "음식 이름: " + foodMenu.getFood() +
                         "\n 칼로리: " + foodMenu.getCalories() + " kcal"+
                         "\n 탄수화물: " + foodMenu.getCarbohydrate() + " g" +
                         "\n 단백질: " + foodMenu.getProtein() + " g" +
@@ -83,7 +82,7 @@ public class DietOutputNutient extends AppCompatActivity {
                         "\n 콜레스테롤: " + foodMenu.getCholesterol() + " mg" +
                         "\n 나트륨: " + foodMenu.getSodium() + " mg" +
                         "\n 설탕 당: " + foodMenu.getSugar() + " g";
-                nutList.setText(nutriateText);
+                nutList.setText(nutriaText);
 
                 /*프로그래스 바 및 칼로리 텍스트 업데이트
                 double totalCalories = totalCalories + foodMenu.getCalories();
@@ -95,9 +94,9 @@ public class DietOutputNutient extends AppCompatActivity {
                 // saveButton 클릭 시 동작 추가
                 Button saveButton = findViewById(R.id.save_diet);
                 saveButton.setOnClickListener(v -> {
-                    foodcal.saveFoodCal(UserSet.getUserId(), foodMenu.getFood(), foodMenu.getCalories(), foodMenu.getCarbohydrate(), foodMenu.getProtein(), foodMenu.getFat(), foodMenu.getCholesterol(), foodMenu.getSodium(), foodMenu.getSugar(), imgName);
-                    Toast.makeText(DietOutputNutient.this, "식단이 저장되었습니다.", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(DietOutputNutient.this, MainActivity2.class);
+                    foodCal.saveFoodCal(UserSet.getUserId(), foodMenu.getFood(), foodMenu.getCalories(), foodMenu.getCarbohydrate(), foodMenu.getProtein(), foodMenu.getFat(), foodMenu.getCholesterol(), foodMenu.getSodium(), foodMenu.getSugar(), imgName);
+                    Toast.makeText(DietOutputNutrient.this, "식단이 저장되었습니다.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(DietOutputNutrient.this, MainActivity2.class);
                     startActivity(intent);
                 });
 
@@ -105,8 +104,7 @@ public class DietOutputNutient extends AppCompatActivity {
 
             @Override
             public void onFoodMenuNotFound() {
-                // 데이터가 없을 때
-                runOnUiThread(() -> Toast.makeText(DietOutputNutient.this, "해당 메뉴를 찾을 수 없습니다.", Toast.LENGTH_LONG).show());
+                runOnUiThread(() -> Toast.makeText(DietOutputNutrient.this, "해당 메뉴를 찾을 수 없습니다.", Toast.LENGTH_LONG).show());
             }
         });
     }
@@ -119,13 +117,16 @@ public class DietOutputNutient extends AppCompatActivity {
 
             // 검색된 결과가 있는지 확인
             if (!foodMenuList.isEmpty()) {
-                // UI 스레드에서 실행
                 Food_menu food = foodMenuList.get(0);
                 runOnUiThread(() -> listener.onFoodMenuRetrieved(food));
             } else {
-                // UI 스레드에서 실행
                 runOnUiThread(listener::onFoodMenuNotFound);
             }
         }).start();
+    }
+
+    public interface OnFoodMenuRetrievedListener {
+        void onFoodMenuRetrieved(Food_menu foodMenu);
+        void onFoodMenuNotFound();
     }
 }
--- app/src/main/java/com/example/dia_v102/utils/OnFoodMenuRetrievedListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.dia_v102.utils;
-
-import com.example.dia_v102.entities.Food_menu;
-
-public interface OnFoodMenuRetrievedListener {
-    void onFoodMenuRetrieved(Food_menu foodMenu);
-    void onFoodMenuNotFound();
-}
